c927ba3d6b8e17f1b7ca80bb6e7bd1cf09118df9
skaman <sandro@skanetwork.com>
2011-11-05 14:47:31 +0100

    Add volume webapi

--- plugin/path/web.py
+++ plugin/path/web.py
@@ -14,6 +14,7 @@ from Cheetah.Template import Template
 
 from Plugins.Extensions.OpenWebif.core.info import getInfo, getBasePath, getWebPublicPath, getWebTemplatesPath
 from Plugins.Extensions.OpenWebif.core.services import getCurrentService, getBouquets, getChannels, getSatellites
+from Plugins.Extensions.OpenWebif.core.volume import getVolumeStatus, setVolumeUp, setVolumeDown, setVolumeMute, setVolume
 from dynamic import DynamicPath
 
 class WebPath(DynamicPath):
@@ -30,6 +31,29 @@ class WebPath(DynamicPath):
 			info = getInfo()
 			service = getCurrentService(self.session)
 			return self.loadTemplate(self.getPath(path + ".xml"), {"info": info, "service": service})
+			
+		elif path == "vol":
+			if "set" not in request.args.keys() or request.args["set"][0] == "state":
+				volume = getVolumeStatus()
+			elif request.args["set"][0] == "up":
+				volume = setVolumeUp()
+			elif request.args["set"][0] == "down":
+				volume = setVolumeDown()
+			elif request.args["set"][0] == "mute":
+				volume = setVolumeMute()
+			elif request.args["set"][0][:3] == "set":
+				try:
+					value = int(request.args["set"][0][3:])
+					volume = setVolume(value)
+				except Exception, e:
+					volume = getVolumeStatus()
+					volume["result"] = False
+					volume["message"] = "Wrong parameter format 'set=%s'. Use set=set15 " % request.args["set"][0]
+			else:
+				volume = getVolumeStatus()
+				volume["result"] = False
+				volume["message"] = "Unknown Volume command %s" % request.args["set"][0]
+			return self.loadTemplate(self.getPath(path + ".xml"), volume)
 			
 		return None
 
