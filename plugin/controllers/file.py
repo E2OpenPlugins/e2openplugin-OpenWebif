ed387abbdee8dc0a29f1b6908102bd940c480448
MiLo <milo-software@users.sourceforge.net>
2011-12-26 13:47:57 +0100

    file.py: Cleanup and simplify
    Simply return a string and let twisted set the content-length and
    other headers. Using write() and finish() is for when you want to
    defer.

--- plugin/controllers/file.py
+++ plugin/controllers/file.py
@@ -10,17 +10,11 @@ from os import path as os_path
 from twisted.web import static, resource, http
 from urllib import unquote, quote
 from Components.config import config
-from Components.Network import iNetwork
 
 class FileController(resource.Resource):
 
 	def __init__(self, path = ""):
 		resource.Resource.__init__(self)
-
-	def formatIp(self, ip):
-		if len(ip) != 4:
-			return None
-		return "%d.%d.%d.%d" % (ip[0], ip[1], ip[2], ip[3])
 
 	def render(self, request):
 		action = "download"
@@ -32,9 +26,7 @@ class FileController(resource.Resource):
 
 			if action != "streamts":
 				if not os_path.exists(filename):
-					request.setResponseCode(http.OK)
-					request.write("File '%s' not found" % (filename))
-					request.finish()
+					return "File '%s' not found" % (filename)
 
 			name = "stream"
 			if "name" in request.args:
@@ -43,25 +35,18 @@ class FileController(resource.Resource):
 				response = "#EXTM3U\nhttp://%s:%s/file?action=download&file=%s" % (request.getRequestHostname(), config.OpenWebif.port.value, quote(filename))
 				request.setHeader("content-disposition", 'inline;filename="%s.m3u"' % name)
 				request.setHeader("content-type", "audio/mpegurl")
-				request.setHeader("content-length", len(response))
-				request.write(response)
-				request.finish()
+				return response
 			elif action == "streamts":
 				response = "#EXTM3U\nhttp://%s:8001/%s\n" % (request.getRequestHostname(), filename)
 				request.setHeader("content-disposition", 'inline;filename="%s.m3u"' % name)
 				request.setHeader("content-type", "audio/mpegurl")
-				request.setHeader("content-length", len(response))
-				request.write(response)
-				request.finish()
+				return response
 			elif action == "delete":
 				request.setResponseCode(http.OK)
-				request.write("TODO: DELETE FILE: %s" % (filename))
-				request.finish()
+				return "TODO: DELETE FILE: %s" % (filename)
 			elif action == "download":
 				request.setHeader("content-disposition", "attachment;filename=\"%s\"" % (filename.split('/')[-1]))
 				rfile = static.File(filename, defaultType = "application/octet-stream")
 				return rfile.render(request)
 		else:
-			request.setResponseCode(http.OK)
-			request.write("Missing file parameter")
-			request.finish()
+			return "Missing file parameter"
