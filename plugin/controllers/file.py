ee32a55f4a7ad72fe17bd09d2ba3b264e5054ece
MiLo <milo-software@users.sourceforge.net>
2011-12-26 12:23:51 +0100

    Fix streaming support
    - Use hostname from client request, this allows the streaming interface
      to work across NAT and other proxies.
    - Avoid using "file" as variable name, it's a builtin function.
    - Using "keys()" on a dictionary will convert it to an array, and thus
      force a linear "in" search. Just use "in" directly on the dict.

--- plugin/controllers/file.py
+++ plugin/controllers/file.py
@@ -21,45 +21,40 @@ class FileController(resource.Resource):
 		if len(ip) != 4:
 			return None
 		return "%d.%d.%d.%d" % (ip[0], ip[1], ip[2], ip[3])
-	
-	def getLocalIPAddress(self):
-		for iface in iNetwork.getConfiguredAdapters():
-			return self.formatIp(iNetwork.getAdapterAttribute(iface, "ip"))
-		return "127.0.0.1"
-		
+
 	def render(self, request):
 		action = "download"
-		if "action" in request.args.keys():
+		if "action" in request.args:
 			action = request.args["action"][0]
 			
-		if "file" in request.args.keys():
-			file = unquote(request.args["file"][0]).decode('utf-8', 'ignore').encode('utf-8')
+		if "file" in request.args:
+			filename = unquote(request.args["file"][0]).decode('utf-8', 'ignore').encode('utf-8')
 
 			if action != "streamts":
-				if not os_path.exists(file):
+				if not os_path.exists(filename):
 					request.setResponseCode(http.OK)
-					request.write("File '%s' not found" % (file))
+					request.write("File '%s' not found" % (filename))
 					request.finish()
-	
+
 			if action == "stream":
 				request.setHeader("content-disposition", "inline;filename=\"stream.m3u\"")
 				request.setHeader("content-type", "audio/mpegurl")
 				request.write("#EXTM3U\n")
-				request.write("http://%s:%s/file?action=download&file=%s" % (self.getLocalIPAddress(), config.OpenWebif.port.value, quote(file)))
+				request.write("http://%s:%s/file?action=download&file=%s" % (request.getRequestHostname(), config.OpenWebif.port.value, quote(filename)))
 				request.finish()
 			elif action == "streamts":
 				request.setHeader("content-disposition", "inline;filename=\"stream.m3u\"")
 				request.setHeader("content-type", "audio/mpegurl")
 				request.write("#EXTM3U\n")
-				request.write("http://%s:%s/%s" % (self.getLocalIPAddress(), '8001', file))
+				request.write("http://%s:8001/%s\n" % (request.getRequestHostname(), filename))
 				request.finish()
 			elif action == "delete":
 				request.setResponseCode(http.OK)
-				request.write("TODO: DELETE FILE: %s" % (file))
+				request.write("TODO: DELETE FILE: %s" % (filename))
 				request.finish()
 			elif action == "download":
-				request.setHeader("content-disposition", "attachment;filename=\"%s\"" % (file.split('/')[-1]))
-				rfile = static.File(file, defaultType = "application/octet-stream")
+				request.setHeader("content-disposition", "attachment;filename=\"%s\"" % (filename.split('/')[-1]))
+				rfile = static.File(filename, defaultType = "application/octet-stream")
 				return rfile.render(request)
 		else:
 			request.setResponseCode(http.OK)
