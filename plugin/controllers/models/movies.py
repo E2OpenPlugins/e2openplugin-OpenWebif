8b30c9f66aacbff7b1c07fde147a6220b21eda25
nobody9 <nobody9b@gmail.com>
2012-01-03 21:58:52 +0100

    add filename escaping in movie list for os_stat
    some ts-files have ugly chars

--- plugin/controllers/models/movies.py
+++ plugin/controllers/models/movies.py
@@ -15,7 +15,7 @@ from Tools.Directories import resolveFilename, SCOPE_HDD
 from Tools.FuzzyDate import FuzzyTime
 from os import stat as os_stat
 from Components.MovieList import MovieList
-
+import re
 def getMovieList(self,directory=resolveFilename(SCOPE_HDD)):
 
 	movieliste = []
@@ -71,7 +71,7 @@ def getMovieList(self,directory=resolveFilename(SCOPE_HDD)):
 		movie['serviceref'] = serviceref.toString()
 		movie['length'] = Len
 		movie['tags'] = info.getInfoString(serviceref, iServiceInformation.sTags)
-		movie['filesize'] = os_stat(filename)[6]
+		movie['filesize'] = os_stat(re.escape(filename)[6])
 		movie['fullname'] = serviceref.toString()
 		movie['DescriptionExtended'] = ext
 		movie['servicename'] = sourceRef.getServiceName().replace('\xc2\x86', '').replace('\xc2\x87', '')
