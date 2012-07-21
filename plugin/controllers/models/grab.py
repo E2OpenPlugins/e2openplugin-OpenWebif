a4e13dd06eaabfb9d0182570f01dc4c2f3d38f75
pieterg <pieterg@gmx.com>
2012-07-21 12:40:52 +0200

    grab: whitespace fixes (trailing tabs)
    ..and sorry about missing user info in the previous commit

--- plugin/controllers/models/grab.py
+++ plugin/controllers/models/grab.py
@@ -18,21 +18,21 @@ class grabScreenshot(resource.Resource):
 		resource.Resource.__init__(self)
 		self.container = eConsoleAppContainer()
 		self.container.appClosed.append(self.grabFinished)
-		
+
 	def render(self, request):
 		self.request = request
 		graboptions = ""
-		
+
 		if "format" in request.args.keys():
 			self.fileformat = request.args["format"][0]
 		else:
 			self.fileformat = "jpg"
-			
+
 		if self.fileformat == "jpg":
 			graboptions += " -j 100"
 		elif self.fileformat == "png":
 			graboptions += " -p"
-				
+
 		if "r" in request.args.keys():
 			size = request.args["r"][0]
 			graboptions += " -r %s" % size
@@ -43,12 +43,12 @@ class grabScreenshot(resource.Resource):
 				graboptions += " -o"
 			elif mode == "video":
 				graboptions += " -v"
-				
+
 		self.filepath = "/tmp/screenshot." + self.fileformat
 		grabcommand = GRAB_PATH + graboptions + " " + self.filepath
-		
+
 		#self.container.execute(grabcommand)
-		
+
 		os.system(grabcommand)
 		self.grabFinished()
 		return server.NOT_DONE_YET
@@ -61,7 +61,7 @@ class grabScreenshot(resource.Resource):
 			self.request.setHeader('Content-Disposition', 'inline; filename=screenshot.%s;' % self.fileformat)
 			self.request.setHeader('Content-Type','image/%s' % fileformat)
 			self.request.setHeader('Content-Length', '%i' % os.path.getsize(self.filepath))
-			
+
 			file = open(self.filepath)
 			self.request.write(file.read())
 			file.close()
@@ -69,5 +69,5 @@ class grabScreenshot(resource.Resource):
 		except Exception, error:
 			self.request.setResponseCode(http.OK)
 			self.request.write("Error creating screenshot:\n %s" % error)
-			self.request.finish()				
-				
+			self.request.finish()
+
