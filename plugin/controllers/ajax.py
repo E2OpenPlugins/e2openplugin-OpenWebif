4410ce64a76ad72965966bfa773cbd1333cd2c69
Persian Professionals <persianpros@yahoo.com>
2013-01-28 04:17:01 +0330

    Update plugin/controllers/ajax.py

--- plugin/controllers/ajax.py
+++ plugin/controllers/ajax.py
@@ -116,6 +116,10 @@ class AjaxController(BaseController):
 			box['brand'] = "vuplus"
 		elif fileExists("/proc/stb/info/azmodel"):
 			box['brand'] = "azbox"
+		elif fileExists("/proc/stb/info/gbmodel"):
+			box['brand'] = "gigablue"
+		elif fileExists("/proc/stb/info/hwmodel"):
+			box['brand'] = "technomate"			
 		return { "box": box }
 		
 	def P_powerstate(self, request):
