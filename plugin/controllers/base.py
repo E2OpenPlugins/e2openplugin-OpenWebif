c5e004927e771da5048605e72c3701d868feb312
Black_64 <black6464@gmail.com>
2013-08-24 19:04:34 +0200

    fix full remote gbquad

--- plugin/controllers/base.py
+++ plugin/controllers/base.py
@@ -193,7 +193,7 @@ class BaseController(resource.Resource):
 			ret["remote"] = "et4x00"
 		elif ret["box"] == "et6500":
 			ret["remote"] = "et6500"
-		elif ret["box"] in ("gb800solo", "gb800se", "gb800ue", "quad"):
+		elif ret["box"] in ("gb800solo", "gb800se", "gb800ue", "gbquad"):
 			ret["remote"] = "gigablue"
 		elif ret["box"] in ("me", "minime"):
 			ret["remote"] = "me"
