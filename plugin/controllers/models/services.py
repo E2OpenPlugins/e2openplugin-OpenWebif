E2OpenPlugins/e2openplugin-OpenWebif

commit 72abca9d04e3318c12e8835a716bad6c7623f8e5

fix current site when STB is in standby

plugin/controllers/models/services.py
@@ -452,56 +452,64 @@ def getEvent(ref, idev):
 
 
 def getChannelEpg(ref, begintime=-1, endtime=-1):
-	ref = unquote(ref)
 	ret = []
 	ev = {}
+	use_empty_ev = False
+	if ref:
+		ref = unquote(ref)
 
-	# Wnen quering EPG we dont need URL, also getPicon doesn't like URL
-	if "://" in ref:
-		ref = ":".join(ref.split(":")[:10]) + "::" + ref.split(":")[-1]
+		# When quering EPG we dont need URL, also getPicon doesn't like URL
+		if "://" in ref:
+			ref = ":".join(ref.split(":")[:10]) + "::" + ref.split(":")[-1]
 
-	picon = getPicon(ref)
-	epgcache = eEPGCache.getInstance()
-	events = epgcache.lookupEvent(['IBDTSENC', (ref, 0, begintime, endtime)])
-	if events is not None:
-		for event in events:
-			ev = {}
-			ev['picon'] = picon
-			ev['id'] = event[0]
-			if event[1]:
-				ev['date'] = "%s %s" % (tstrings[("day_" + strftime("%w", (localtime(event[1]))))], strftime("%d.%m.%Y", (localtime(event[1]))))
-				ev['begin'] = strftime("%H:%M", (localtime(event[1])))
-				ev['begin_timestamp'] = event[1]
-				ev['duration'] = int(event[2] / 60)
-				ev['duration_sec'] = event[2]
-				ev['end'] = strftime("%H:%M",(localtime(event[1] + event[2])))
-				ev['title'] = filterName(event[3])
-				ev['shortdesc'] = event[4]
-				ev['longdesc'] = event[5]
-				ev['sref'] = ref
-				ev['sname'] = filterName(event[6])
-				ev['tleft'] = int (((event[1] + event[2]) - event[7]) / 60)
-				if ev['duration_sec'] == 0:
-					ev['progress'] = 0
+		picon = getPicon(ref)
+		epgcache = eEPGCache.getInstance()
+		events = epgcache.lookupEvent(['IBDTSENC', (ref, 0, begintime, endtime)])
+		if events is not None:
+			for event in events:
+				ev = {}
+				ev['picon'] = picon
+				ev['id'] = event[0]
+				if event[1]:
+					ev['date'] = "%s %s" % (tstrings[("day_" + strftime("%w", (localtime(event[1]))))], strftime("%d.%m.%Y", (localtime(event[1]))))
+					ev['begin'] = strftime("%H:%M", (localtime(event[1])))
+					ev['begin_timestamp'] = event[1]
+					ev['duration'] = int(event[2] / 60)
+					ev['duration_sec'] = event[2]
+					ev['end'] = strftime("%H:%M",(localtime(event[1] + event[2])))
+					ev['title'] = filterName(event[3])
+					ev['shortdesc'] = event[4]
+					ev['longdesc'] = event[5]
+					ev['sref'] = ref
+					ev['sname'] = filterName(event[6])
+					ev['tleft'] = int (((event[1] + event[2]) - event[7]) / 60)
+					if ev['duration_sec'] == 0:
+						ev['progress'] = 0
+					else:
+						ev['progress'] = int(((event[7] - event[1]) * 100 / event[2]) *4)
+					ev['now_timestamp'] = event[7]
 				else:
-					ev['progress'] = int(((event[7] - event[1]) * 100 / event[2]) *4)
-				ev['now_timestamp'] = event[7]
-			else:
-				ev['date'] = 0
-				ev['begin'] = 0
-				ev['begin_timestamp'] = 0
-				ev['duration'] = 0
-				ev['duration_sec'] = 0
-				ev['end'] = 0
-				ev['title'] = "N/A"
-				ev['shortdesc'] = ""
-				ev['longdesc'] = ""
-				ev['sref'] = ref
-				ev['sname'] = filterName(event[6])
-				ev['tleft'] = 0
-				ev['progress'] = 0
-				ev['now_timestamp'] = 0
-			ret.append(ev)
+					use_empty_ev = True
+					ev['sref'] = ref
+	else:
+		use_empty_ev = True
+		ev['sref'] = ""
+		
+	if use_empty_ev:
+		ev['date'] = 0
+		ev['begin'] = 0
+		ev['begin_timestamp'] = 0
+		ev['duration'] = 0
+		ev['duration_sec'] = 0
+		ev['end'] = 0
+		ev['title'] = "N/A"
+		ev['shortdesc'] = ""
+		ev['sname'] = ""
+		ev['longdesc'] = ""
+		ev['tleft'] = 0
+		ev['progress'] = 0
+		ev['now_timestamp'] = 0
+		ret.append(ev)
 
 	return { "events": ret, "result": True }
 

commit 50ce779fc1f44d1f23369a0e44626f7d8404959c

fix channel EPG site (bug introduced with commit 72abca9d04e3318c12e8835a716bad6c7623f8e5 )

plugin/controllers/models/services.py
@@ -488,6 +488,7 @@ def getChannelEpg(ref, begintime=-1, endtime=-1):
 					else:
 						ev['progress'] = int(((event[7] - event[1]) * 100 / event[2]) *4)
 					ev['now_timestamp'] = event[7]
+					ret.append(ev)
 				else:
 					use_empty_ev = True
 					ev['sref'] = ref