bad17b01b3e2c8fa42a209273228d562f08cec59
atvcaptain <captain@web.de>
2013-12-26 20:19:23 +0100

    [vu] fix transcoding settings

--- plugin/controllers/models/config.py
+++ plugin/controllers/models/config.py
@@ -66,7 +66,7 @@ def getZapStream():
 	}
 
 def getJsonFromConfig(cnf):
-	if cnf.__class__.__name__ == "ConfigSelection" or cnf.__class__.__name__ == "ConfigSelectionNumber":
+	if cnf.__class__.__name__ == "ConfigSelection" or cnf.__class__.__name__ == "ConfigSelectionNumber" or cnf.__class__.__name__ == "TconfigSelection":
 		if type(cnf.choices.choices) == dict:
 			choices = []
 			for choice in cnf.choices.choices:
@@ -106,7 +106,7 @@ def getJsonFromConfig(cnf):
 			"type": "number",
 			"current": cnf.value
 		}
-	elif cnf.__class__.__name__ == "ConfigInteger":
+	elif cnf.__class__.__name__ == "ConfigInteger" or cnf.__class__.__name__ == "TconfigInteger":
 		return {
 			"result": True,
 			"type": "number",
@@ -141,7 +141,7 @@ def saveConfig(path, value):
 			cnf.value = values
 		elif cnf.__class__.__name__ == "ConfigNumber":
 			cnf.value = int(value)
-		elif  cnf.__class__.__name__ == "ConfigInteger":
+		elif  cnf.__class__.__name__ == "ConfigInteger" or cnf.__class__.__name__ == "TconfigInteger":
 			cnf_min = int(cnf.limits[0][0])
 			cnf_max = int(cnf.limits[0][1])
 			cnf_value = int(value)
@@ -247,6 +247,9 @@ class ConfigFiles:
 			xmldata = setupdom.getroot()
 			for section in xmldata.findall("setup"):
 				configs = []
+				requires = section.get("requires")
+				if requires and not SystemInfo.get(requires, False):
+					continue;
 				key = section.get("key")
 				if key not in self.allowedsections:
 					showOpenWebIF = section.get("showOpenWebIF")
