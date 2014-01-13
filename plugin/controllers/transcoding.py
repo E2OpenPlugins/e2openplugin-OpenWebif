1269a8cdf3fb9e4945514f3cd990325d7fe93ee5
Andy Blackburn <andrew.blackburn644@btinternet.com>
2014-01-13 16:04:28 +0000

    [Transcoding] fix API to work with OE-Alliance version TransCodeSetup. ( note this is lock to encoder 0 for now, i need to add support for multiple encoder choice)

--- plugin/controllers/transcoding.py
+++ plugin/controllers/transcoding.py
@@ -22,8 +22,8 @@ class TranscodingController(resource.Resource):
 		try:
 			transcoding = config.plugins.transcodingsetup.transcoding
 			port = config.plugins.transcodingsetup.port
-			bitrate = config.plugins.transcodingsetup.bitrate
-			framerate = config.plugins.transcodingsetup.framerate
+			bitrate = config.plugins.transcodingsetup.encoder[0].bitrate
+			framerate = config.plugins.transcodingsetup.encoder[0].framerate
 		except KeyError:
 			return '<?xml version="1.0" encoding="UTF-8" ?><e2simplexmlresult><e2state>false</e2state><e2statetext>Transcoding Plugin is not installed or your STB does not support transcoding</e2statetext></e2simplexmlresult>'
 		
@@ -42,39 +42,39 @@ class TranscodingController(resource.Resource):
 						new_bitrate = int(request.args[arg][0])
 					except ValueError:
 						return '<?xml version="1.0" encoding="UTF-8" ?><e2simplexmlresult><e2state>false</e2state><e2statetext>wrong argument for bitrate</e2statetext></e2simplexmlresult>'
-					if new_bitrate < int(bitrate.limits[0][0]):
-						new_bitrate = int(bitrate.limits[0][0])
-					elif new_bitrate > int(bitrate.limits[0][1]):
-						new_bitrate = int(bitrate.limits[0][1])
-					if new_bitrate != config.plugins.transcodingsetup.bitrate.value:
-						config.plugins.transcodingsetup.bitrate.value = new_bitrate
+					if not new_bitrate in bitrate.choices:
+						new_bitrate = bitrate.value
+					if new_bitrate != config.plugins.transcodingsetup.encoder[0].bitrate.value:
+						config.plugins.transcodingsetup.encoder[0].bitrate.value = new_bitrate
 						config_changed = True
 				elif arg == "framerate":
 					new_framerate = request.args[arg][0]
 					if not new_framerate in framerate.choices:
 						new_framerate = framerate.value
-					if new_framerate != config.plugins.transcodingsetup.framerate.value:
-						config.plugins.transcodingsetup.framerate.value = new_framerate
+					if new_framerate != config.plugins.transcodingsetup.encoder[0].framerate.value:
+						config.plugins.transcodingsetup.encoder[0].framerate.value = new_framerate
 						config_changed = True
 				elif arg == "port":
 					new_port = request.args[arg][0]
-					if not new_port in port.choices:
-						new_port = port.value
+					if new_port < int(port.limits[0][0]):
+						new_port = int(port.limits[0][0])
+					elif new_port > int(port.limits[0][1]):
+						new_port = int(port.limits[0][1])
 					if new_port != config.plugins.transcodingsetup.port.value:
 						config.plugins.transcodingsetup.port.value = new_port
 						config_changed = True
 			if config_changed:
 				config.plugins.transcodingsetup.save()
-		ports = ""
-		for port_available in port.choices:
-			ports += port_available + ", "
-		ports = ports.rstrip(', ')
+		port_min = str(port.limits[0][0])
+		port_max = str(port.limits[0][1])
 		framerates = ""
 		for framerate_available in framerate.choices:
 			framerates += framerate_available + ", "
 		framerates = framerates.rstrip(', ')
-		bitrate_min = str(bitrate.limits[0][0])
-		bitrate_max = str(bitrate.limits[0][1])
+		bitrates = ""
+		for bitrate_available in bitrate.choices:
+			bitrates += bitrate_available + ", "
+		bitrates = bitrates.rstrip(', ')
 
 		return """<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 				<e2configs>
@@ -84,12 +84,12 @@ class TranscodingController(resource.Resource):
 					</e2config>
 					<e2config>
 						<e2configname>port</e2configname>
-						<e2configchoices>%s</e2configchoices>
+						<e2configlimits>%s-%s</e2configlimits>
 						<e2configvalue>%s</e2configvalue>
 					</e2config>
 					<e2config>
 						<e2configname>bitrate</e2configname>
-						<e2configlimits>%s-%s</e2configlimits>
+						<e2configchoices>%s</e2configchoices>
 						<e2configvalue>%s</e2configvalue>
 					</e2config>
 					<e2config>
@@ -98,6 +98,6 @@ class TranscodingController(resource.Resource):
 						<e2configvalue>%s</e2configvalue>
 					</e2config>
 				</e2configs>""" %      (str(config.plugins.transcodingsetup.transcoding.value),
-							ports, str(config.plugins.transcodingsetup.port.value),
-							bitrate_min, bitrate_max, str(config.plugins.transcodingsetup.bitrate.value),
-							framerates, str(config.plugins.transcodingsetup.framerate.value))
+							port_min, port_max, str(config.plugins.transcodingsetup.port.value),
+							bitrates, str(config.plugins.transcodingsetup.encoder[0].bitrate.value),
+							framerates, str(config.plugins.transcodingsetup.encoder[0].framerate.value))
