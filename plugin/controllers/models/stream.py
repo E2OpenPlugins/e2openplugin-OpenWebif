0f1ff062050d4a566825df0c1a9ffff86a3bd7a8
Andy Blackburn <andrew.blackburn644@btinternet.com>
2014-01-29 13:59:54 +0000

    Fix boxname typo for Transcoding.

--- plugin/controllers/models/stream.py
+++ plugin/controllers/models/stream.py
@@ -48,7 +48,7 @@ def getStream(session, request, m3ufile):
 	portNumber = config.OpenWebif.streamport.value
 	info = getInfo()
 	model = info["model"]
-	if model in ("solo2", "duo2", "Sezam Marvel", "Xpeed LX-3", "gbquad", "gbquadplus"):
+	if model in ("Solo²", "Duo²", "Sezam Marvel", "Xpeed LX-3", "Quad", "Quad Plus"):
 		if "device" in request.args :
 			if request.args["device"][0] == "phone" :
 				portNumber = config.plugins.transcodingsetup.port.value
@@ -79,7 +79,7 @@ def getTS(self, request):
 		portNumber = config.OpenWebif.port.value
 		info = getInfo()
 		model = info["model"]
-		if model in ("solo2", "duo2", "Sezam Marvel", "Xpeed LX3", "gbquad", "gbquadplus"):
+		if model in ("Solo²", "Duo²", "Sezam Marvel", "Xpeed LX-3", "Quad", "Quad Plus"):
 			if "device" in request.args :
 				if request.args["device"][0] == "phone" :
 					portNumber = config.plugins.transcodingsetup.port.value
