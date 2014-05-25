81ed2f1b6ebceeb4153e78bbad19a38d14e67788
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2014-05-25 11:17:15 +0200

    Backport an oe-a fix
    ... concerning broken certs/keys, as seen here:
    http://openpli.org/forums/topic/20164-openwebif-e2openplugins/page-60#entry425009

--- plugin/httpserver.py
+++ plugin/httpserver.py
@@ -171,6 +171,35 @@ def HttpdStart(session):
 				BJregisterService('https',httpsPort)
 			except CannotListenError:
 				print "[OpenWebif] failed to listen on Port", httpsPort
+			except: # THIS EXCEPTION IS ONLY CATCHED WHEN CERT FILES ARE BAD ( look below for error )
+				print "[OpenWebif] failed to get valid cert files. ( It could occure bad file save or format, removing... )"
+				# removing bad files
+				os.remove("/etc/enigma2/cert.pem")
+				os.remove("/etc/enigma2/key.pem")
+				# regenerate new ones
+				installCertificates(session)
+				# restart
+				try:
+					context = ssl.DefaultOpenSSLContextFactory(KEY_FILE, CERT_FILE)
+
+					if config.OpenWebif.https_clientcert.value == True:
+						ctx = context.getContext()
+						ctx.set_verify(
+							SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT,
+							verifyCallback
+							)
+						ctx.load_verify_locations(CA_FILE)
+
+					if has_ipv6 and fileExists('/proc/net/if_inet6') and version.major >= 12:
+						# use ipv6
+						listener.append( reactor.listenSSL(httpsPort, site, context, interface='::') )
+					else:
+						# ipv4 only
+						listener.append( reactor.listenSSL(httpsPort, site, context) )
+					print "[OpenWebif] started on", httpsPort
+					BJregisterService('https',httpsPort)
+				except CannotListenError:
+					print "[OpenWebif] failed to listen on Port", httpsPort
 
 #Streaming requires listening on 127.0.0.1:80	
 		if port != 80:
