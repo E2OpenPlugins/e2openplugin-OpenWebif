4342063a323ae1b47580105726c76113985ca396
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2014-05-27 10:16:30 +0200

    Partial revert
    Don't see the reason for the previous change.

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-from os import environ as os_environ
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 import gettext
 
 PluginLanguageDomain = "OpenWebif"
 PluginLanguagePath = "Extensions/OpenWebif/locale"
 
 def localeInit():
-    lang = language.getLanguage()[:2]
-    os_environ['LANGUAGE'] = lang
-    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
-
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 def _(txt):
-    t = gettext.dgettext(PluginLanguageDomain, txt)
-    if t == txt:
-        t = gettext.gettext(txt)
-    return t
-
+	t = gettext.dgettext(PluginLanguageDomain, txt)
+	if t == txt:
+		t = gettext.gettext(txt)
+	return t
 
 localeInit()
 language.addCallback(localeInit)
