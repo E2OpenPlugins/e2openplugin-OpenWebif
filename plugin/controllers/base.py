4d9b21b35cf77d3d525e8d49a1f46241c87a6ebf
bonkel <bonkelchen@gmail.com>
2014-06-02 12:56:09 +0200

    fix

--- plugin/controllers/base.py
+++ plugin/controllers/base.py
@@ -183,8 +183,10 @@ class BaseController(resource.Resource):
 			ret["remote"] = "et5x00"
 		elif ret["box"] in ("et4x00", "et4000"):
 			ret["remote"] = "et4x00"
-		elif ret["box"].startswith("gb"):
+		elif ret["box"] == "gbquad":
 			ret["remote"] = "gigablue"
+		elif ret["box"] == "gbquadplus":
+			ret["remote"] = "gbquadplus"
 		elif ret["box"] == "et6500":
 			ret["remote"] = "et6500"
 		elif ret["box"] in ("et8x00", "et8000", "et1x000", "et10000"):
