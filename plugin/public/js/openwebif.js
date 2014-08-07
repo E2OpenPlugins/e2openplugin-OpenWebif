d3a751bdfccc17cebec0aae517713e46a14d2ae1
Jörg Bleyel <jbleyel@gmx.net>
2014-08-07 18:36:15 +0200

    fix typo

--- plugin/public/js/openwebif.js
+++ plugin/public/js/openwebif.js
@@ -726,7 +726,7 @@ function editTimer(serviceref, begin, end) {
 							} else {
 								$('#timername').removeClass('ui-state-disabled');
 								$('#timerbegin').removeClass('ui-state-disabled');
-								$("#dirname option").removeAttr(''disabled');
+								$("#dirname option").removeAttr('disabled');
 								$("#bouquet_select option").removeAttr('disabled');
 							}
 							$('#timerbegin').prop('readonly', r);
