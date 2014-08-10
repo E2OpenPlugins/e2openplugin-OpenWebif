78c04c86ced087b4f093a9e208ca2f574f72b66c
Koivo <koivo@live.de>
2014-08-10 16:37:41 +0200

    revert enable gzip feature

--- plugin/controllers/base.py
+++ plugin/controllers/base.py
@@ -74,7 +74,6 @@ class BaseController(resource.Resource):
 		path = self.path
 		isJson = self.isJson
 		isCustom = self.isCustom
-		isGZ = self.isGZ
 
 		if self.path == "":
 			self.path = "index"
@@ -103,18 +102,10 @@ class BaseController(resource.Resource):
 			elif self.isJson:
 #				if not self.suppresslog:
 #					print "[OpenWebif] page '%s' ok (json)" % request.uri
-				supported=[]
+#	TODO: Discuss to use GZIP for all requests if the browser accepts gzip encoding
 				if self.isGZ:
-					acceptHeaders = request.requestHeaders.getRawHeaders('Accept-Encoding', [])
-					supported = ','.join(acceptHeaders).split(',')
-				if 'gzip' in supported:
-					encoding = request.responseHeaders.getRawHeaders('Content-Encoding')
-					if encoding:
-						encoding = '%s,gzip' % ','.join(encoding)
-					else:
-						encoding = 'gzip'
-					request.responseHeaders.setRawHeaders('Content-Encoding',[encoding])
 					compstr = self.compressBuf(json.dumps(data))
+					request.setHeader('Content-Encoding', 'gzip')
 					request.setHeader('Content-Length', '%d' % len(compstr))
 					request.write(compstr)
 				else:
@@ -159,7 +150,6 @@ class BaseController(resource.Resource):
 		self.path = path
 		self.isJson = isJson
 		self.isCustom = isCustom
-		self.isGZ = isGZ
 
 		return server.NOT_DONE_YET
 
