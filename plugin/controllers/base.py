47b5e60defc5d2c42f1d7d6855c82431cc79bceb
atvcaptain <captain@web.de>
2014-09-05 22:54:51 +0200

    * Prepare adding the box' name to the header

--- plugin/controllers/base.py
+++ plugin/controllers/base.py
@@ -19,7 +19,7 @@ from twisted.web import server, http, static, resource, error
 from Cheetah.Template import Template
 
 from models.info import getInfo, getBasePath, getPublicPath, getViewsPath
-from models.config import getCollapsedMenus, getRemoteGrabScreenshot, getZapStream, getConfigsSections
+from models.config import getCollapsedMenus, getRemoteGrabScreenshot, getZapStream, getConfigsSections, getShowName, getCustomName, getBoxName
 
 import imp
 import sys
@@ -185,6 +185,11 @@ class BaseController(resource.Resource):
 		ret['remotegrabscreenshot'] = getRemoteGrabScreenshot()['remotegrabscreenshot']
 		ret['configsections'] = getConfigsSections()['sections']
 		ret['zapstream'] = getZapStream()['zapstream']
+		ret['showname'] = getShowName()['showname']
+		ret['customname'] = getCustomName()['customname']
+		ret['boxname'] = getBoxName()['boxname']
+		if not ret['boxname'] or not ret['customname']:
+			ret['boxname'] = getInfo()['brand']+" "+getInfo()['model']
 		ret['box'] = getBoxType()
 		ret["remote"] = rc_model.getRcFolder()
 
