4fc5673811a11dc87d279f36d5145d77cd603558
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2014-11-17 17:59:15 +0100

    Merge some of plnicks changes to e2openlugin-OpenWebif

--- plugin/controllers/models/info.py
+++ plugin/controllers/models/info.py
@@ -338,11 +338,16 @@ def getStatusInfo(self):
 		if len(curEvent[3].decode('utf-8')) > 220:
 			statusinfo['currservice_description'] = curEvent[3].decode('utf-8')[0:220].encode('utf-8') + "..."
 		statusinfo['currservice_station'] = serviceHandlerInfo.getName(serviceref).replace('\xc2\x86', '').replace('\xc2\x87', '')
+		full_desc = statusinfo['currservice_name'] + '\n'
+		full_desc += statusinfo['currservice_begin'] + " - " + statusinfo['currservice_end']  + '\n\n'
+		full_desc += event.getExtendedDescription().replace('\xc2\x86', '').replace('\xc2\x87', '').replace('\xc2\x8a', '\n')
+		statusinfo['currservice_fulldescription'] = full_desc
 	else:
 		statusinfo['currservice_name'] = "N/A"
 		statusinfo['currservice_begin'] = ""
 		statusinfo['currservice_end'] = ""
 		statusinfo['currservice_description'] = ""
+		statusinfo['currservice_fulldescription'] = "N/A"
 		if serviceref:
 			statusinfo['currservice_serviceref'] = serviceref.toString()
 			statusinfo['currservice_station'] = serviceHandlerInfo.getName(serviceref).replace('\xc2\x86', '').replace('\xc2\x87', '')
