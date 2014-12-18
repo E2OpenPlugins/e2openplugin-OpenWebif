1cc29f8a94c181af6cef83f7560fad77867c133a
meo <lupomeo@hotmail.com>
2014-12-18 10:32:57 +0100

    owibranding.py: removed unused check for tpm before to check the hardware model - this check is unuseful because it only assign the variable orgdream that is not currently used (maybe forgot to remove) - this check cause an error with images like BlackHole that pass the Etpm import but have not all tpm functions - this check is duplicated because there is the Tpm check in the right place: in Dmm block condition

--- plugin/controllers/models/owibranding.py
+++ plugin/controllers/models/owibranding.py
@@ -65,9 +65,6 @@ def getAllInfo():
 	brand = "unknown"
 	model = "unknown"
 	procmodel = "unknown"
-	orgdream = 0
-	if tpmloaded:
-		orgdream = tpm_check()
 	
 	if fileExists("/proc/stb/info/hwmodel"):
 		brand = "DAGS"
