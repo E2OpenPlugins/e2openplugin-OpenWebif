225c1898eb80d84c64a30dd9bc556b8267377cd5
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2015-06-16 18:59:03 +0200

    Merge master

--- plugin/controllers/models/info.py
+++ plugin/controllers/models/info.py
@@ -291,7 +291,7 @@ def getInfo():
 		else:
 			size = "%d MB" % size
 
-		iecsize = hdd.diskSize()
+		iecsize = hdd.diskSize() / float(500) * 512
 		# Harddisks > 1000 decimal Gigabytes are labelled in TB
 		if iecsize > 1000000:
 			iecsize = (iecsize + 50000) // float(100000) / 10
