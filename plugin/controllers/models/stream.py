35362b32b1f5012987cd2ae2c27ccf42bdae9d5c
Captain <captain.onboard@web.de>
2015-11-25 13:07:27 +0100

    add solo4k transcoding

--- plugin/controllers/models/stream.py
+++ plugin/controllers/models/stream.py
@@ -52,7 +52,7 @@ def getStream(session, request, m3ufile):
 	machinebuild = info["machinebuild"]
 	transcoder_port = None
 	args = ""
-	if model in ("Solo²", "Duo²", "Solo SE", "Quad", "Quad Plus") or machinebuild in ('dags3', 'dags4'):
+	if model in ("Solo4K", "Solo²", "Duo²", "Solo SE", "Quad", "Quad Plus") or machinebuild in ('dags3', 'dags4'):
 		try:
 			transcoder_port = int(config.plugins.transcodingsetup.port.value)
 		except StandardError:
