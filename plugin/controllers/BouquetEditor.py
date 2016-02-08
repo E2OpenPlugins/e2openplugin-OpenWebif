fb054c159c04506d2e770385720d80fbcae88364
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2016-02-08 17:16:10 +0100

    Sync with oe-a

--- plugin/controllers/BouquetEditor.py
+++ plugin/controllers/BouquetEditor.py
@@ -556,10 +556,10 @@ class BouquetEditor(Source):
 			files.append(checkfile)
 			files.append("/etc/enigma2/bouquets.tv")
 			files.append("/etc/enigma2/bouquets.radio")
-			files.append("/etc/enigma2/userbouquet.favourites.tv")
-			files.append("/etc/enigma2/userbouquet.favourites.radio")
+			#files.append("/etc/enigma2/userbouquet.favourites.tv")
+			#files.append("/etc/enigma2/userbouquet.favourites.radio")
 			files.append("/etc/enigma2/lamedb")
-			for xml in ("/etc/enigma2/cables.xml", "/etc/enigma2/terrestrial.xml", "/etc/enigma2/satellites.xml", "/etc/tuxbox/cables.xml", "/etc/tuxbox/terrestrial.xml", "/etc/tuxbox/satellites.xml"):
+			for xml in ("/etc/tuxbox/cables.xml", "/etc/tuxbox/terrestrial.xml", "/etc/tuxbox/satellites.xml"):
 				if path.exists(xml):
 					files.append(xml)
 			if config.ParentalControl.configured.value:
