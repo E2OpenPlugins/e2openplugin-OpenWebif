892a611cc33f3ea4e9272cb14045a0aeb89e5b63
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2016-11-08 20:22:04 +0100

    [I18N] Adjust a string and translate it (de-DE)

--- plugin/local.py
+++ plugin/local.py
@@ -415,7 +415,7 @@ tstrings = {'mo': _("Mo"),
 	'tstr_ow_browser_settings': _("OpenWebif Browser Settings"),
 	'tstr_ow_settings': _("OpenWebif Settings"),
 	'tstr_theme': _("Theme"),
-	'tstr_show_picon_in_channel_list': _("Show Picon in channel list"),
+	'tstr_show_picon_in_channel_list': _("Show Picons in channel list"),
 	'tstr_spinner': _("Spinner"),
 	'display_duration_s': _("Display duration (s)"),
 
