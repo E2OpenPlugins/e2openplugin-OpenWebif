872110d8016c2a89299c58c06a594ffd834818c6
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2016-11-23 18:48:00 +0100

    Fix password check
    Allowing empty passes would render the whole thing useless.

--- plugin/httpserver.py
+++ plugin/httpserver.py
@@ -365,7 +365,7 @@ class AuthResource(resource.Resource):
 					cpass = getspnam(user)[1]
 				except:
 					return False
-			return not cpass or crypt(passwd, cpass) == cpass
+			return crypt(passwd, cpass) == cpass
 		return False
 
 #
