c75f0889a4f793175415e5313cf68fe8946c57a1
Jörg Bleyel <jbleyel@gmx.net>
2017-05-12 22:31:50 +0200

    Update min.js

--- sourcefiles/js/openwebif.js
+++ sourcefiles/js/openwebif.js
@@ -1,6 +1,6 @@
 //******************************************************************************
 //* openwebif.js: openwebif base module
-//* Version 1.2.3
+//* Version 1.2.4
 //******************************************************************************
 //* Copyright (C) 2011-2017 E2OpenPlugins
 //*
@@ -18,6 +18,7 @@
 //* V 1.2.1 - fix multiepg
 //* V 1.2.2 - improve epgsearch
 //* V 1.2.3 - fix add at from multiepg
+//* V 1.2.4 - fix screenshot refresh
 //*
 //* Authors: skaman <sandro # skanetwork.com>
 //* 		 meo
