7ef568d871f95aab1d105387e872097d9da07f30
doubleO8 <wb008@hdm-stuttgart.de>
2017-08-29 18:54:01 +0200

    fix: typo

--- plugin/controllers/file.py
+++ plugin/controllers/file.py
@@ -99,5 +99,5 @@ class FileController(resource.Resource):
 				data.append({"result": True,"dirs": directories,"files": files})
 			else:
 				data.append({"result": False,"message": "path %s not exits" % (path)})
-			request.setHeader("content-type", "application/jsons")
+			request.setHeader("content-type", "application/json")
 			return json.dumps(data)
