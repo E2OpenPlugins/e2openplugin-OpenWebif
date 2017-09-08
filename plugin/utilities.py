212b3600aa58f6a49ec58a90f50b4748a54e45fb
doubleO8 <wb008@hdm-stuttgart.de>
2017-09-08 22:32:28 +0200

    more string manipulation utilities; docstrings improved

--- plugin/utilities.py
+++ plugin/utilities.py
@@ -10,10 +10,11 @@ def lenient_decode(value, encoding=None):
 	"""
 	Decode an encoded string and convert it to an unicode string.
 
-	:param value: input value
-	:param encoding: string encoding, defaults to utf-8
-	:return: decoded value
-	:rtype: unicode
+	Args:
+		value: input value
+		encoding: string encoding, defaults to utf-8
+	Returns:
+		(unicode) decoded value
 
 	>>> lenient_decode("Hallo")
 	u'Hallo'
@@ -29,6 +30,40 @@ def lenient_decode(value, encoding=None):
 		encoding = 'utf_8'
 
 	return value.decode(encoding, 'ignore')
+
+
+def lenient_force_utf_8(value):
+	"""
+
+	Args:
+		value: input value
+	Returns:
+		(basestring) utf-8 encoded value
+
+	>>> isinstance(lenient_force_utf_8(''), basestring)
+	True
+	>>> lenient_force_utf_8(u"Hallo")
+	'Hallo'
+	>>> lenient_force_utf_8("HällöÜ")
+	'H\\xc3\\xa4ll\\xc3\\xb6\\xc3\\x9c'
+	"""
+	return lenient_decode(value).encode('utf_8')
+
+
+def sanitise_filename_slashes(value):
+	"""
+
+	Args:
+		value: input value
+	Returns:
+		value w/o multiple slashes
+
+	>>> in_value = "///tmp/x/y/z"
+	>>> expected = re.sub("^/+", "/", "///tmp/x/y/z")
+	>>> sanitise_filename_slashes(in_value) == expected
+	True
+	"""
+	return re.sub(MANY_SLASHES_REGEX, '/', value)
 
 
 if __name__ == '__main__':
