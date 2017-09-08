b908522312a9f0d8c559946e481fa13f96b740f7
doubleO8 <wb008@hdm-stuttgart.de>
2017-09-08 22:43:20 +0200

    add 'status quo FileController responses' documenting testsuite

---
+++ testsuite/status_quo_file_controller.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import unittest
+import uuid
+
+import requests
+
+TARGET_URL_BASE_FMT = 'http://{host}/file'
+
+from movie_files_testsuite import dump_disclaimer
+from movie_files_testsuite import ENV_VAR, ENV_VAL_FALLBACK
+
+
+class TestEnigma2FileAPICalls(unittest.TestCase):
+	"""
+	This test suite is used to document the behaviour of /file endpoint as
+	of 2017-09-08.
+	"""
+
+	def setUp(self):
+		self.enigma2_host = os.environ.get(ENV_VAR, ENV_VAL_FALLBACK)
+		self.file_url = TARGET_URL_BASE_FMT.format(host=self.enigma2_host)
+
+	def test_etc_passwd(self):
+		params = {
+			"file": "/etc/passwd"
+		}
+		req = requests.get(self.file_url, params=params)
+		print("Tried to fetch {!r}".format(req.url))
+		self.assertTrue(len(req.text) > 20)
+		self.assertEqual(200, req.status_code)  # should this be allowed at all?
+
+	def test_nonexisting_file(self):
+		randy = uuid.uuid4().hex
+		params = {
+			"file": randy
+		}
+		req = requests.get(self.file_url, params=params)
+		print("Tried to fetch {!r}".format(req.url))
+		self.assertEquals("File '/home/root/{:s}' not found".format(randy),
+						  req.text)
+		self.assertEqual(200, req.status_code)
+
+	def test_nonexisting_path(self):
+		randy = uuid.uuid4().hex
+		params = {
+			"dir": randy
+		}
+		req = requests.get(self.file_url, params=params)
+		print("Tried to fetch {!r}".format(req.url))
+		minimal_expectation = {"message": "path {:s} not exits".format(randy),
+							   "result": False}
+		result = req.json()[0]
+		for key in minimal_expectation:
+			self.assertEquals(minimal_expectation[key], result.get(key))
+		self.assertEqual(200, req.status_code)
+
+	def test_empty_glob_result(self):
+		randy = uuid.uuid4().hex
+		params = {
+			"dir": '/etc',
+			"pattern": randy
+		}
+		req = requests.get(self.file_url, params=params)
+		print("Tried to fetch {!r}".format(req.url))
+		minimal_expectation = {"dirs": [], "result": True}
+		result = req.json()[0]
+		for key in minimal_expectation:
+			self.assertEquals(minimal_expectation[key], result.get(key))
+		self.assertEqual(200, req.status_code)
+
+	def test_nonempty_glob_result(self):
+		params = {
+			"dir": '/etc',
+			"pattern": 'opkg*'
+		}
+		req = requests.get(self.file_url, params=params)
+		print("Tried to fetch {!r}".format(req.url))
+		minimal_expectation = {"dirs": ['/etc/opkg/'], "result": True,
+							   "files": []}
+		result = req.json()[0]
+		for key in minimal_expectation:
+			self.assertEquals(minimal_expectation[key], result.get(key))
+		self.assertEqual(200, req.status_code)
+
+
+if __name__ == '__main__':
+	dump_disclaimer()
+	unittest.main()
