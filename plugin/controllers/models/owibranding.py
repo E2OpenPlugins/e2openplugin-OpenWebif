edcaab9dbe7f7c0fec28d18c0d5e49fc209fd159
technl <tech@openpli.org>
2017-11-08 19:56:39 +0100

    Add Vu+ VuUno4kSE

--- plugin/controllers/models/owibranding.py
+++ plugin/controllers/models/owibranding.py
@@ -426,6 +426,8 @@ def getAllInfo():
 		remote = "vu_duo2"
 	elif procmodel == "ultimo":
 		remote = "vu_ultimo"
+	elif procmodel == "vuuno4kse":
+		remote = "vuuno4kse"
 	elif procmodel == "e3hd":
 		remote = "e3hd"
 	elif procmodel in ("et9x00", "et9000", "et9100", "et9200", "et9500"):
