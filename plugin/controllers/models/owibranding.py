5f21d6855332f4a7c4dd238aeb58c23335db19cb
Athanasios Oikonomou <athoik@gmail.com>
2018-08-18 16:22:28 +0300

    Edision OS Nino Pro support

--- plugin/controllers/models/owibranding.py
+++ plugin/controllers/models/owibranding.py
@@ -353,6 +353,8 @@ def getAllInfo():
 				model = "OS Nino"
 			elif procmodel == "osninoplus":
 				model = "OS Nino+"
+			elif procmodel == "osninopro":
+				model = "OS Nino Pro"
 			else:
 				model = procmodel
 		elif procmodel == "h3":
@@ -565,6 +567,8 @@ def getAllInfo():
 		remote = procmodel
 	elif procmodel.startswith("osm"):
 		remote = "osmini"
+	elif procmodel.startswith("osninopr"):
+		remote = "edision3"
 	elif procmodel.startswith("osninopl"):
 		remote = "edision2"
 	elif procmodel.startswith("osn"):
