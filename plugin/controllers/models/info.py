0956866a658a172b196ddf7225444f7f27cb7d50
ims <ims21@users.sourceforge.net>
2020-06-14 21:13:18 +0200

    fixed bracket

--- plugin/controllers/models/info.py
+++ plugin/controllers/models/info.py
@@ -162,7 +162,7 @@ def getAdapterIPv6(ifname):
 				tmpaddr = ""
 				tmp = line.split()
 				if ifname == tmp[5]:
-					tmpaddr = ":".join([tmp[0][i:i + 4] for i in list(range(0, len(tmp[0]), 4)]))
+					tmpaddr = ":".join([tmp[0][i:i + 4] for i in list(range(0, len(tmp[0]), 4))])
 
 					if firstpublic is None and (tmpaddr.startswith('2') or tmpaddr.startswith('3')):
 						firstpublic = normalize_ipv6(tmpaddr)
