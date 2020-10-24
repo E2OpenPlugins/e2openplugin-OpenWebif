b258003d18402d501833ef5886315a5d1c728498
wedebe <9741693+wedebe@users.noreply.github.com>
2020-10-24 20:21:09 +0100

    UX - accessibility (#1079)
    * Fixed input focus underline colour
    
    * Added Bouquet Editor channel list loading visual indicator
    
    * Fixed merge issue
    
    * Accessibility improvements
    
    This change:
    - removes the single-biggest accessibility crime across the internet (outline: none)
    - enlarges modal `X` button
    - enlarges header dropdown icons
    - inverts text color on yellow skin elements (white -> off-black)
    - aligns form text inputs and dropdowns vertically
    - englarges dropdown chevrons

--- sourcefiles/themes/absb/plugins/bootstrap-select/js/bootstrap-select.js
+++ sourcefiles/themes/absb/plugins/bootstrap-select/js/bootstrap-select.js
@@ -438,7 +438,7 @@
           inputGroup = this.$element.parent().hasClass('input-group') ? ' input-group-btn' : '',
           autofocus = this.autofocus ? ' autofocus' : '';
       // Elements
-      var header = this.options.header ? '<div class="popover-title"><button type="button" class="close" aria-hidden="true">&times;</button>' + this.options.header + '</div>' : '';
+      var header = this.options.header ? '<div class="popover-title"><button type="button" class="close" aria-hidden="true"><i class="material-icons material-icons-centered">close</i></button>' + this.options.header + '</div>' : '';
       var searchbox = this.options.liveSearch ?
       '<div class="bs-searchbox">' +
       '<input type="text" class="form-control" autocomplete="off"' +
