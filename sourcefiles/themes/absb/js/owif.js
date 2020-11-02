6f8cda16756f3a57073b1a8aa7fa904133bb70df
wedebe <9741693+wedebe@users.noreply.github.com>
2020-11-02 23:07:03 +0000

    AutoTimer code modernisation, part ? of ~ (Responsive)
    This change:
    - fixes population of channels/bouquets/tags associated with an AT
    - removes some jQuery dependence around c/b/t dropdowns
    - removes some legacy dependence in favour of ES6 (more modern JS)
    - utilises parcelJS to bundle browser support for ES6 (entry-app.js)
    
    This change does not:
    - seem to list existing tags (#1092)
    - currently allow for new tags to be added

--- sourcefiles/themes/absb/js/owif.js
+++ sourcefiles/themes/absb/js/owif.js
@@ -12,10 +12,79 @@ class STB {
 
 class API { 
   constructor() {}
+
+  async getTags() {
+    let response = await fetch('/api/gettags');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      return await response.json();
+    }
+  }
+
+  async getAllServices() {
+    let response = await fetch('/api/getallservices');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      const jsonResponse = await response.json();
+      let allChannels = [];
+      const bouquets = jsonResponse['services'].map((bouquet) => {
+        const bouquetChannels = bouquet['subservices'].map((channel) => {
+          const name = channel['servicename'];
+          const sRef = channel['servicereference'];
+          const isMarker = (sRef.indexOf('1:64:') > -1);
+          /*
+            1:0: - tv?
+            4097: - url?
+            1:134:1 - (encodeURIComponent)
+          */
+          return {
+            name: name,
+            sRef: sRef,
+            disabled: isMarker,
+          }
+        });
+
+        allChannels = allChannels.concat(bouquetChannels);
+
+        return {
+          name: bouquet['servicename'],
+          sRef: bouquet['servicereference'],
+          channels: bouquetChannels,
+        }
+      });
+
+      return await {
+        channels: allChannels,
+        bouquets: bouquets,
+      };
+    }
+  }
+
 }
 
 class GUI { 
   constructor() {}
+
+  /* TODO: i10n */
+  choicesConfig = {
+    removeItemButton: true,
+    duplicateItemsAllowed: false,
+    resetScrollPosition: false,
+    shouldSort: false,
+    placeholder: true,
+    renderSelectedChoices: 'always',
+    itemSelectText: '',
+    /*
+      loadingText: 'Loading...',
+      placeholderValue: null,
+      searchPlaceholderValue: null,
+      noResultsText: 'No results found',
+      notagChoicesext: 'No choices to choose from',
+      callbackOnInit: null,
+    */
+  };
   
   fullscreen(state, el) {
     if (state === true) {
@@ -32,6 +101,44 @@ class GUI {
       });
     }
   } 
+
+  populateAutoTimerOptions() {
+    const populatedChoices = {};
+    const selectChoicesAttr = 'data-select-choices';
+    const selectChoicesElements = document.querySelectorAll(`[${selectChoicesAttr}]`);
+    const api = new API();
+
+    selectChoicesElements.forEach((el) => {
+      populatedChoices[el.getAttribute(`${selectChoicesAttr}`)] = new Choices(el, this.choicesConfig);
+    });
+
+    api.getTags().then((result) => {
+      populatedChoices['tags'].setChoices([]);
+    }).catch(e => console.warn(e));
+    
+    api.getAllServices().then((result) => {
+      const opts = result['bouquets'].map((bouquet) => {
+        return {
+          label: bouquet.name,
+          choices: bouquet.channels,
+        }
+      });
+      populatedChoices['bouquets'].setChoices(
+        result['bouquets'],
+        'sRef',
+        'name',
+        false,
+      );
+      populatedChoices['channels'].setChoices(
+        opts,
+        'sRef',
+        'name',
+        false,
+      );
+    }).catch(e => console.warn(e));
+
+    return populatedChoices;
+  }
 } 
 
 class OWIF { 
@@ -42,4 +149,4 @@ class OWIF {
   }
 }
 
-const owif = new OWIF();
+window.owif = new OWIF();
