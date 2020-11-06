03f4e849461dc830851f6409d6c43b5881496a1a
wedebe <9741693+wedebe@users.noreply.github.com>
2020-11-06 21:57:30 +0000

    Updated `modern` (responsive) asset paths

--- plugin/controllers/root.py
+++ plugin/controllers/root.py
@@ -56,9 +56,9 @@ class RootController(BaseController):
 		if os.path.exists(getPublicPath('mobile')):
 			self.putChild2("mobile", MobileController(session))
 			self.putChild2("m", static.File(getPublicPath() + "/mobile"))
-		for static_val in ('js', 'css', 'static', 'images', 'fonts'):
+		for static_val in ('classic', 'shared', 'static', 'images'):
 			self.putChild2(static_val, static.File(six.ensure_binary(getPublicPath() + '/' + static_val)))
-		for static_val in ('themes', 'webtv', 'vxg'):
+		for static_val in ('modern', 'webtv', 'vxg'):
 			if os.path.exists(getPublicPath(static_val)):
 				self.putChild2(static_val, static.File(six.ensure_binary(getPublicPath() + '/' + static_val)))
 
