b24dfdaa901e0d887619b33202741a279b35871b
wedebe <9741693+wedebe@users.noreply.github.com>
2020-11-11 22:19:58 +0000

    Icons, dark theme tweaks, date formatting fixes and tweaks
    This change:
    - adds timer status icons to MultiEPG, replacing red 'timer' text
    - adds visual indicator around timered event
    - darkens page load background for better dark theme effect
    - updates AutoTimer icon for consistency
    - fixes Date Recorded format on movie list
    - fixes alternate row background colour for dark theme on MultiEPG

--- plugin/controllers/models/services.py
+++ plugin/controllers/models/services.py
@@ -672,7 +672,11 @@ def getEventDesc(ref, idev, encode=True):
 
 
 def getTimerEventStatus(event, eventLookupTable):
-	#ValueError
+	# Check if an event has an associated timer. Unfortunately
+	# we cannot simply check against timer.eit, because a timer
+	# does not necessarily have one belonging to an epg event id.
+
+	#catch ValueError
 	startTime = event[eventLookupTable.index('B')]
 	endTime = event[eventLookupTable.index('B')] + event[eventLookupTable.index('D')] - 120
 	serviceref = event[eventLookupTable.index('R')]
@@ -686,12 +690,14 @@ def getTimerEventStatus(event, eventLookupTable):
 			if timer.begin <= startTime and timer.end >= endTime:
 				if timer.disabled:
 					return { 
-						'isEnabled': 0 
+						'isEnabled': 0,
+						'basicStatus': 'timer disabled'
 					}
 				else:
 					return { 
 						'isEnabled': 1, 
-						'isZapOnly': int(timer.justplay)
+						'isZapOnly': int(timer.justplay),
+						'basicStatus': 'timer'
 					}
 	return None
 
@@ -1038,29 +1044,13 @@ def getSearchSimilarEpg(ref, eventid, encode=False):
 
 
 def getMultiEpg(self, ref, begintime=-1, endtime=None, Mode=1):
-	# Check if an event has an associated timer. Unfortunately
-	# we cannot simply check against timer.eit, because a timer
-	# does not necessarily have one belonging to an epg event id.
-	def getTimerEventStatus(event):
-		startTime = event[1]
-		endTime = event[1] + event[6] - 120
-		serviceref = event[4]
-		if serviceref not in timerlist:
-			return ''
-		for timer in timerlist[serviceref]:
-			if timer.begin <= startTime and timer.end >= endTime:
-				if timer.disabled:
-					return 'timer disabled'
-				else:
-					return 'timer'
-		return ''
-
 	ret = OrderedDict()
 	services = eServiceCenter.getInstance().list(eServiceReference(ref))
 	if not services:
 		return {"events": ret, "result": False, "slot": None}
 
-	search = ['IBTSRND']
+	eventLookupTable = 'IBTSRND'
+	search = [eventLookupTable]
 	for service in services.getContent('S'):
 		if endtime:
 			search.append((service, 0, begintime, endtime))
@@ -1097,13 +1087,20 @@ def getMultiEpg(self, ref, begintime=-1, endtime=None, Mode=1):
 			lastevent = offset + 86399
 
 		for event in events:
+			timer = getTimerEventStatus(event, eventLookupTable)
+			# timerStatus is kept for backwards compatibility
+			basicStatus = ''
+			if timer:
+				basicStatus = timer['basicStatus']
+
 			ev = {}
 			ev['id'] = event[0]
 			ev['begin_timestamp'] = event[1]
 			ev['title'] = event[2]
 			ev['shortdesc'] = convertDesc(event[3])
 			ev['ref'] = event[4]
-			ev['timerStatus'] = getTimerEventStatus(event)
+			ev['timerStatus'] = basicStatus
+			ev['timer'] = timer
 			if Mode == 2:
 				ev['duration'] = event[6]
 
--- plugin/controllers/views/responsive/ajax/renderevtblock.py
+++ plugin/controllers/views/responsive/ajax/renderevtblock.py
@@ -9,14 +9,19 @@ from Plugins.Extensions.OpenWebif.controllers.i18n import tstrings
 class renderEvtBlock:
 	def __init__(self):
 		self.template = """
-		<article class="epg__event event" data-ref="%s" data-id="%s" data-toggle="modal" data-target="#EventModal" onclick="loadeventepg('%s', '%s'); return false;">
-			<time class="epg__time--start">%s%s</time>
-			<span class="epg__title title">%s</span>
+		<article onclick="loadeventepg('%s', '%s'); return false;" class="epg__event event %s" data-ref="%s" data-id="%s" data-toggle="modal" data-target="#EventModal">
+			<time class="epg__time--start">%s</time>
+			<span class="epg__title title">
+				%s
+				<span class="epg__timer-status">%s</span>
+			</span>
 			%s
 		</article>
 		"""
 
 	def render(self, event):
+		eventCssClass = ''
+
 		if event['title'] != event['shortdesc']:
 			shortdesc = '<summary class="epg__desc desc">%s</summary>' % (
 				event['shortdesc']
@@ -24,19 +29,26 @@ class renderEvtBlock:
 		else:
 			shortdesc = ''
 
-		if event['timerStatus'] != '':
-			timerEventSymbol = '<span class="epg__timer %s">%s</span>' % (
-				event['timerStatus'], tstrings['timer']
-			)
+		timer = event['timer']
+		if timer:
+			eventCssClass = eventCssClass + ' event--has-timer'
+			if timer['isEnabled']:
+				timerEventSymbol = '<i class="material-icons material-icons-centered">alarm_on</i>'
+			else:
+				timerEventSymbol = '<i class="material-icons material-icons-centered">alarm_off</i>'
 		else:
 			timerEventSymbol = ''
+
+		sRef = quote(event['ref'], safe=' ~@#$&()*!+=:;,.?/\'')
+
 		return self.template % (
-			quote(event['ref'], safe=' ~@#$&()*!+=:;,.?/\''),
 			event['id'],
+			sRef,
+			eventCssClass,
+			sRef,
 			event['id'],
-			quote(event['ref'], safe=' ~@#$&()*!+=:;,.?/\''),
 			strftime("%H:%M", localtime(event['begin_timestamp'])),
-			timerEventSymbol,
 			event['title'],
+			timerEventSymbol,
 			shortdesc
 		)
