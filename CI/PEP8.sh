fd881793d07f8d18c47f6c62f4ab6b2a3e266d16
Jörg Bleyel <jbleyel@gmx.net>
2021-04-18 13:24:26 +0200

    autopep8 remove TRAVIS_BUILD_NUMBER

--- CI/PEP8.sh
+++ CI/PEP8.sh
@@ -13,56 +13,56 @@ echo "PEP8 double aggressive E401"
 autopep8 . -a -a -j 0 --recursive --select=E401 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E401 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E401"
 
 echo ""
 echo "PEP8 double aggressive E701, E70 and E502"
 autopep8 . -a -a -j 0 --recursive --select=E701,E70,E502 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E701, E70 and E502 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E701, E70 and E502"
 
 echo ""
 echo "PEP8 double aggressive E251 and E252"
 autopep8 . -a -a -j 0 --recursive --select=E251,E252 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E251 and E252 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E251 and E252"
 
 echo ""
 echo "PEP8 double aggressive E20 and E211"
 autopep8 . -a -a -j 0 --recursive --select=E20,E211 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E20 and E211 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E20 and E211"
 
 echo ""
 echo "PEP8 double aggressive E22, E224, E241, E242 and E27"
 autopep8 . -a -a -j 0 --recursive --select=E22,E224,E241,E242,E27 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E22, E224, E241, E242 and E27 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E22, E224, E241, E242 and E27"
 
 echo ""
 echo "PEP8 double aggressive E225 ~ E228 and E231"
 autopep8 . -a -a -j 0 --recursive --select=E225,E226,E227,E228,E231 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E225 ~ E228 and E231 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E225 ~ E228 and E231"
 
 echo ""
 echo "PEP8 double aggressive E301 ~ E306"
 autopep8 . -a -a -j 0 --recursive --select=E301,E302,E303,E304,E305,E306 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive E301 ~ E306 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive E301 ~ E306"
 
 echo ""
 echo "PEP8 double aggressive W291 ~ W293 and W391"
 autopep8 . -a -a -j 0 --recursive --select=W291,W292,W293,W391 --in-place
 git add -u
 git add *
-git commit -m "PEP8 double aggressive W291 ~ W293 and W391 (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "PEP8 double aggressive W291 ~ W293 and W391"
 
 echo ""
 finish=$(date +"%s")
