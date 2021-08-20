c4f6603cf34d8dfd312a8c0a45dbb38bd3d4b1ad
wedebe <9741693+wedebe@users.noreply.github.com>
2021-08-20 21:08:02 +0100

    Migrate to Webpack - update CI script

--- CI/npm.sh
+++ CI/npm.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
 echo ""
-echo "NPM Build JS AND CSS"
-echo ""
-echo "Changing py files, please wait ..." 
+echo "NPM build JS and CSS"
 begin=$(date +"%s")
 
 echo ""
-echo "Run npm to minimize CSS and JS"
+echo "Run npm to compile and minimise JS and CSS files"
 cd sourcefiles
 npm install
+npm ci --prefix modern
 npm run build-css
 npm run build-classic-js
 npm run build-js
 cd ..
 git add -u
 git add *
-git commit -m "Update JS/CSS min files"
+git commit -m "Compile JS & CSS assets"
 
 echo ""
 finish=$(date +"%s")
 timediff=$(($finish-$begin))
 echo -e "Change time was $(($timediff / 60)) minutes and $(($timediff % 60)) seconds."
-echo -e "Fast changing would be less than 1 minute."
 echo ""
-echo "NPM Build Done!"
+echo "NPM build done!"
 echo ""
